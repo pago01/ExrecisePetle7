b58f51f099c49f5bf342f8cf6be18ceba07a7921
ACTFYZZ <ACTFYZZ@PL10006677>
2023-08-23 12:14:53 +0200

    Add project files.

---
+++ ExrecisePetle7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace ExercisePetle6
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int a = 0;
+            string b = "";
+            Console.WriteLine("Podaj dlugosc przekatnej");
+            Int32.TryParse(Console.ReadLine(), out a);
+            //Calculation and drawing
+                // Upper part and horizontal diagonal
+                for(int i = a % 2; i <=a; i=i+2)                    
+                {
+                    b = "";
+                    for (int j = 1; j <= (a-i)/2; j++)
+                    {
+                        b = b + " ";
+                    }
+                    for (int j = 1; j <=i; j++)
+                    {
+                        b = b + "*";
+                    }
+                    Console.WriteLine(b);
+                }
+                //Lower part
+                for (int i = a-2; i >= 1; i=i-2)
+                {
+                    b = "";                   
+                    for (int j = 1; j <= (a-i)/2; j++)
+                    {
+                        b = b + " ";
+                    }
+                    for (int j = 1; j <= i; j++)
+                    {
+                        b = b + "*";
+                    }
+                    Console.WriteLine(b);
+                }
+        }
+    }
+}
